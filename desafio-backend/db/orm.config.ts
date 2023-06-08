b977cab1a1985567e7d2aa87ef650ade2c0ef807
VitorMagno <vmcg@ic.ufal.br>
2023-06-07 22:07:38 -0300

    config

--- desafio-backend/db/orm.config.ts
+++ desafio-backend/db/orm.config.ts
@@ -3,10 +3,10 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const config: TypeOrmModuleOptions = {
     type:'postgres',
     username:'postgres',
-    password:'admin123',
+    password:'123456',
     port:5432,
     host:'localhost',
-    database:'desafio-back',
+    database:'postgres',
     synchronize:true,
     entities:['../src/**/*.entity{.ts,.js}'],
 }
